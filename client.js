55c9d05cea76b383c79733d42b9605ba13e25b94
maxgud86 <maxgud86@gmail.com>
2021-05-27 21:58:58 -0600

    Working!
    added table column components

--- client.js
+++ client.js
@@ -13,10 +13,46 @@ var pokemonTableRow = Vue.component(
     template: `
       <tr class="pokemon-table-row">
         <td>{{pokemon.name}}</td>
+        <td>{{pokemon.id}}</td>
         <td>{{pokemon.stamina}}</td>
         <td>{{pokemon.attack}}</td>
         <td>{{pokemon.defense}}</td>
       </tr>
+    `
+  }
+);
+
+var pokemonTableColumnHeader = Vue.component(
+  //convention for naming Vue components
+  //note line template and table row class name
+  'pokemon-table-column-header',
+  {
+    props: {
+      label: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+    },
+    methods: {
+      emitSort: function(name, direction){
+        console.log("tableColumnHeader button was clicked", name, direction);
+        this.$emit('sort', {name, direction});
+      },
+    },
+    template: `
+      <th>
+        <span>{{label}}</span>
+        <button
+          @click="emitSort(name,'forward')"
+        >Up</button>
+        <button
+          @click="emitSort(name,'backward')"
+        >Down</button>
+      </th>
     `
   }
 );
@@ -39,6 +75,12 @@ var pokemonTable = Vue.component(
         default: 'Pokemon table'
       }
     },
+    methods: {
+      emitSort: function(value){
+        console.log("pokemonTable emitted Sort!", value);
+        this.$emit('sort', value);
+      },
+    },
     template: `
       <table
         class="pokemon-table"
@@ -52,10 +94,31 @@ var pokemonTable = Vue.component(
             >{{title}}</th>
           </tr>
           <tr>
-            <th>Name</th>
-            <th>Stamina</th>
-            <th>Attack</th>
-            <th>Defense</th>
+            <pokemon-table-column-header
+              label="Pokemon Name"
+              name="name"
+              @sort="emitSort"
+            ></pokemon-table-column-header>
+            <pokemon-table-column-header
+              label="ID"
+              name="id"
+              @sort="emitSort"
+            ></pokemon-table-column-header>
+            <pokemon-table-column-header
+              label="Stamina"
+              name="stamina"
+              @sort="emitSort"
+            ></pokemon-table-column-header>
+            <pokemon-table-column-header
+              label="Attack"
+              name="attack"
+              @sort="emitSort"
+            ></pokemon-table-column-header>
+            <pokemon-table-column-header
+              label="Defense"
+              name="defense"
+              @sort="emitSort"
+            ></pokemon-table-column-header>
           </tr>
         </thead>
         <tbody>
@@ -71,33 +134,41 @@ var pokemonTable = Vue.component(
   }
 );
 
+var pokemonList = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    stamina: 111,
+    attack: 112,
+    defense: 96
+  },
+  {
+    id: 150,
+    name: 'Mewtwo',
+    stamina: 214,
+    attack: 300,
+    defense: 182
+  },
+  {
+    id: 145,
+    name: 'Zapados',
+    stamina: 207,
+    attack: 253,
+    defense: 185
+  },
+]
 
 var app = new Vue({
   el: '#app',
   data: {
-    pokemonList: [
-      {
-        id: 25,
-        name: 'Pikachu',
-        stamina: 111,
-        attack: 112,
-        defense: 96
-      },
-      {
-        id: 150,
-        name: 'Mewtwo',
-        stamina: 214,
-        attack: 300,
-        defense: 182
-      },
-      {
-        id: 145,
-        name: 'Zapados',
-        stamina: 207,
-        attack: 253,
-        defense: 185
-      },
-    ]
+    pokemonList: JSON.parse(JSON.stringify(pokemonList))
+  },
+  methods: {
+    handleSortEvent: function (sortValues) {
+      var sortingMethod = tableSortingMethods[sortValues.name][sortValues.direction];
+      this.pokemonList.sort(sortingMethod);
+      console.log('OMG the parent is listening', arguments);
+    }
   },
   template: `
     <div class="app">
@@ -105,6 +176,7 @@ var app = new Vue({
       <pokemon-table
         :list="pokemonList"
         title="Best Pokemon Ever"
+        @sort="handleSortEvent"
       ></pokemon-table>
     </div>
   `,
---
+++ sorting.js
@@ -0,0 +1,59 @@
+var sortNumberBackward = function (a,b) {
+    return b-a;
+};
+var sortNumberForward = function (a,b) {
+    return a-b;
+};
+
+var sortStringBackward = function (a,b) {
+    return b.localeCompare(a);
+};
+var sortStringForward = function (a,b) {
+    return a.localeCompare(b);
+};
+
+var sortObjectByNameForward = function (a,b) {
+    return a.name.localeCompare(b.name);
+};
+
+var sortObjectByNameBackwards = function (a,b) {
+    return b.name.localeCompare(a.name);
+};
+
+var createObjectSortingFunction = function (propertyName, sortingFunction) {
+  return function(a,b) {
+    var aValue = a[propertyName];
+    var bValue = b[propertyName];
+    return sortingFunction(aValue, bValue);
+  }
+}
+
+var tableColumns = [
+  'name',
+  'stamina',
+  'attack',
+  'defense',
+];
+
+var tableSortingMethods = {
+  id:{
+    forward:createObjectSortingFunction('id', sortNumberForward),
+    backward:createObjectSortingFunction('id', sortNumberBackward),
+  },
+  name:{
+    forward:createObjectSortingFunction('name', sortStringForward),
+    backward:createObjectSortingFunction('name', sortStringBackward),
+  },
+  stamina:{
+    forward:createObjectSortingFunction('stamina', sortNumberForward),
+    backward:createObjectSortingFunction('stamina', sortNumberBackward),
+  },
+  attack:{
+    forward:createObjectSortingFunction('attack', sortNumberForward),
+    backward:createObjectSortingFunction('attack', sortNumberBackward),
+  },
+  defense:{
+    forward:createObjectSortingFunction('defense', sortNumberForward),
+    backward:createObjectSortingFunction('defense', sortNumberBackward),
+  },
+}
